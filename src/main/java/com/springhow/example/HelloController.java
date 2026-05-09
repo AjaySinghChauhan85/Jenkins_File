715243fa664847438d4f257c7945e12f27e313ed
Ajay Singh Chauhan <107171838+AjaySinghChauhan85@users.noreply.github.com>
2026-05-09 14:59:35 +0530

    Update HelloController.java

--- src/main/java/com/springhow/example/HelloController.java
+++ src/main/java/com/springhow/example/HelloController.java
@@ -1,13 +1,27 @@
+// package com.springhow.example;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class HelloController {
+
+//     @GetMapping("/")
+//     public String hello() {
+//         return "Hello India";
+//     }
+// }
+
 package com.springhow.example;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello India";
+    public String home() {
+        return "index";
     }
 }
